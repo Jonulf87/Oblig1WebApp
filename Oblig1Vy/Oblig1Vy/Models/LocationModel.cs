3a7501cc165479adb97dda85c33c276a3a2264e9
Jonulf87 <42869267+Jonulf87@users.noreply.github.com>
2019-09-20 12:27:40 +0200

    Opprettet en modell

---
+++ Oblig1Vy/Oblig1Vy/Models/LocationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Oblig1Vy.Models
+{
+    public class LocationModel
+    {
+        public int Id { get; set; }
+        public string LocationName { get; set; }
+
+    }
+}
